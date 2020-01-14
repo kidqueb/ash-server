1b2e026c913d4adf81f8334bb205a868ff24a2ca
Nick Quebbeman <n.quebb@gmail.com>
2020-01-13 16:48:22 -0800

    kill extra newline

--- lib/ash_server_web/views/changeset_view.ex
+++ lib/ash_server_web/views/changeset_view.ex
@@ -13,7 +13,6 @@ defmodule AshServerWeb.ChangesetView do
     end)
   end
 
-
   def render("error.json", %{changeset: changeset}) do
     # When encoded, the changeset returns its errors
     # as a JSON object. So we just pass it forward.
